7f3952be42d5bb7fae1906083257a269aea787ce
Ryan Mears <ryan.mears@apple.com>
2011-12-11 20:59:27 -0800

    first update

--- lottery/lottery/main.m
+++ lottery/lottery/main.m
@@ -16,6 +16,20 @@ int main (int argc, const char * argv[])
         // insert code here...
         NSLog(@"Hello, World!");
         
+        NSMutableArray *array;
+        array = [[NSMutableArray alloc] init];
+        int i;
+        
+        for (i = 0; i < 10; i++) {
+            NSNumber *newNumber = [[NSNumber alloc] initWithInt:(i * 3)];
+            [array addObject:newNumber];
+        }
+        
+        for (i = 0; i < 10; i++) {
+            NSNumber *numberToPrint = [array objectAtIndex:i];
+            NSLog(@"The number at index %d is %@", i, numberToPrint);
+        }
+        
     }
     return 0;
 }
