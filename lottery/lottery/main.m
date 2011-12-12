05c8f0e5dd4ab8c9e9002698a6a69d5a23ae1d41
Ryan Mears <ryan.mears@apple.com>
2011-12-11 20:44:30 -0800

    Initial add of the lottery project

---
+++ lottery/lottery/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  lottery
+//
+//  Created by Ryan Mears on 12/11/11.
+//  Copyright (c) 2011 Apple Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
