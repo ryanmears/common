cb40a1b59a60c5b980f0d4d588a09864371e7f84
Ryan Mears <ryan.mears@gmail.com>
2014-04-21 21:05:26 -0700

    load images from array

--- ImageGalleryBasic/ImageGalleryBasic/IGBViewController.m
+++ ImageGalleryBasic/ImageGalleryBasic/IGBViewController.m
@@ -20,26 +20,40 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     NSLog(@"self.view bounds: %@", NSStringFromCGRect([self.view bounds]));
+
+    NSArray *imageNames = @[@"Cloud Sea.jpg", @"cronkite.jpg", @"gull.jpg"];
     
     //Debug colors to identify view hierarchy
     self.view.backgroundColor = [UIColor greenColor];
     
     UIScrollView *imageGallery = [[UIScrollView alloc] init];
     imageGallery.frame = [[self view] bounds];
-    imageGallery.contentSize = CGSizeMake(640, 536);
+    imageGallery.contentSize = CGSizeMake(320*imageNames.count, 536);
     imageGallery.pagingEnabled = YES;
     [[self view] addSubview:imageGallery];
     
-    UIImage *testImage = [UIImage imageNamed:@"Cloud Sea.jpg"];
-    UIImage *testImage2 = [UIImage imageNamed:@"cronkite.jpg"];
-
-    UIImageView *testImageView = [[UIImageView alloc] initWithImage:testImage];
-    testImageView.frame = [self.view bounds];
-    [imageGallery addSubview:testImageView];
-
-    UIImageView *testImageView2 = [[UIImageView alloc] initWithImage:testImage2];
-    testImageView2.frame = CGRectMake(320, 0, 320, 568);
-    [imageGallery addSubview:testImageView2];
+    //Loop through imageNames array
+    //1. create image & image view
+    //2. if this is the first image, set its frame to the upper left
+    //3. if this is an additional image, calculate it's X coord based on its position in the array
+    for (int i = 0; i < imageNames.count; i++)
+    {
+        UIImage *image = [UIImage imageNamed:imageNames[i]];
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+        
+        if (i == 0)
+        {
+            imageView.frame = CGRectMake(0, 0, 320, 568);
+        } else
+        {
+            imageView.frame = CGRectMake(320*i, 0, 320, 568);
+        }
+        
+        [imageGallery addSubview:imageView];
+        
+    }
+    
+    NSLog(@"imageGallery.subviews: %@", imageGallery.subviews);
     
     //Debug colors to identify view hierarchy
     self.view.backgroundColor = [UIColor greenColor];
